c9c1f67b7cda957b56230d2d32f0a693572df33d
Vladislav <45735840+Vlad-Vargan@users.noreply.github.com>
2020-05-20 21:26:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -62,4 +62,4 @@ def rest_3():
     )
 
 if __name__ == "__main__":
-    app.run(debug=False, use_reloader=True, host= '0.0.0.0')
+    app.run(debug=False, use_reloader=True, host= '0.0.0.0', port=8080)
