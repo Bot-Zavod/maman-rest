45b9c79245f9dd81c9c446f5317d0d2b8bd8102d
Vlad-Vargan <blach.smith.2000@gmail.com>
2020-05-20 21:23:47 +0300

    new csv menu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import csv
+import codecs
 import requests
 import os
 
@@ -9,18 +10,25 @@ IMAGE_FOLDER = 'static'
 
 app.config['UPLOAD_FOLDER'] = IMAGE_FOLDER
 
+
 def aggregate_by_first(input_list):
     categories = set([el[0] for el in input_list])
-    categ_dict = {key:list(filter(lambda x: x[0] == key, input_list)) for key in categories}
-    categ_dict = {key:[el[1:] for el in categ_dict[key]] for key in categ_dict.keys()}
+    categ_dict = {key: list(filter(lambda x: x[0] == key, input_list)) 
+                        for key in categories}
+    categ_dict = {key: [el[1:] for el in categ_dict[key]] 
+                        for key in categ_dict.keys()}
     return categ_dict
 
-def get_dishes(csv_path = 'maman-rest/static/example.csv'):
-    with open(csv_path, encoding='utf-16') as csvfile:
+
+def get_dishes(csv_path='static/maman_fontan.csv'):
+    # f = codecs.open(filename=csv_path, mode='r', encoding='utf-16')
+    # with open(csv_path, encoding='utf-16') as csvfile:
+    with codecs.open(filename=csv_path, mode='r', encoding='utf-8') as csvfile:
         menu = list(csv.reader(csvfile))
         menu = aggregate_by_first(menu[1:])
         menu = {key:aggregate_by_first(menu[key]) for key in menu.keys()}
         return menu
+
 
 @app.route("/", methods=["GET", "POST"])
 def rest_default():
@@ -29,6 +37,7 @@ def rest_default():
         data=get_dishes(), page_type="sea"
     )
 
+
 @app.route("/sea", methods=["GET", "POST"])
 def rest_1():
     return render_template(
@@ -36,12 +45,14 @@ def rest_1():
         data=get_dishes(), page_type="sea"
     )
 
+
 @app.route("/fontan", methods=["GET", "POST"])
 def rest_2():
     return render_template(
         'index.html',
         data=get_dishes(), page_type="fontan"
     )
+
 
 @app.route("/test", methods=["GET", "POST"])
 def rest_3():
